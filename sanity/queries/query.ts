debc3973daa137b184360fd3f66b63935577555e
Arunkumar132 <arun132003@gmail.com>
2025-09-19 13:56:48 +0530

    Added successfully

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -1,11 +1,19 @@
 import { defineQuery } from "next-sanity";
 
 const LATEST_BLOG_QUERY = defineQuery(
-    `*[_type == 'blog' && isLatest == true] | order(name asc){
+  `*[_type == 'blog' && isLatest == true] | order(name asc){
     ...,
     blogcategories[]->{
-    title
-    }}`
+      title
+    }
+  }`
 );
 
-export { LATEST_BLOG_QUERY };
+const DEAL_PRODUCTS = defineQuery(
+  `*[_type == 'product' && status == 'hot'] | order(name asc){
+    ...,
+    "categories": categories[]->title
+  }`
+);
+
+export { LATEST_BLOG_QUERY, DEAL_PRODUCTS };
