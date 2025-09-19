bfc9d16f2177f6aeed13f66f4872b149d389dda5
kanishkmaheswar <kanishkmaheswaran4704@gmail.com>
2025-09-19 14:23:19 +0530

    blog

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,33 +1,65 @@
 import type { Metadata } from "next";
+
 import "../globals.css";
+
 import Footer from "@/components/Footer";
 
+
+
 import { ClerkProvider } from "@clerk/nextjs";
+
 import Header from "@/components/Header";
 
+
+
 export const metadata: Metadata = {
+
   title: {
+
     default: "Shopping car accessories",
+
     template: "%s | MotoZoop",
+
   },
+
   description:
+
     "IT is a website for car accessories, your one-stop shop for all things automotive.",
+
 };
 
+
+
 export default function RootLayout({
+
   children,
+
 }: Readonly<{
+
   children: React.ReactNode;
+
 }>) {
+
   return (
+
     <ClerkProvider>
+
       <html lang="en">
+
         <body className="font-poppins antialiased min-h-screen flex flex-col">
+
           <Header />
+
           <main className="flex-1">{children}</main>
+
           <Footer />
+
         </body>
+
       </html>
+
     </ClerkProvider>
-  );
+
+  );
+
 }
